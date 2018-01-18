90a40ff07b986eef4ac64fd60e5660ce200a1606
Jonathon Racz <jdracz@umich.edu>
2018-01-18 15:08:03 -0500

    Add rudimentary module files

---
+++ harmonAIze.cpp
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+   Copyright 2017 Jonathon Racz
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+  ==============================================================================
+*/
+
+#ifdef HARMONAIZE_H_INCLUDED
+ /* When you add this cpp file to your project, you mustn't include it in a file where you've
+    already included any other headers - just put it inside a file on its own, possibly with your config
+    flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+    header files that the compiler may be using.
+ */
+ #error "Incorrect use of JUCE cpp file"
+#endif
