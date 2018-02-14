4a1b0fba0a9048ff444bdcd836bc900d7bb231d1
Landon Kartes <landonkartes@gmail.com>
2018-02-14 14:00:50 -0500

    pass MIDI back from python

--- harmonaize/midiinterchange/InterchangeTest/Source/python.py
+++ harmonaize/midiinterchange/InterchangeTest/Source/python.py
@@ -6,4 +6,8 @@ def openFile(p):
     b = BytesIO(p)
     midifile = MidiFile()
     midifile._load(b)
-    return "done"
+    for message in midifile:
+        print(message)
+    f = BytesIO()
+    midifile.save(file=f)
+    return f.getvalue()
