a50ded939b0eb2221997333de5d6047d94f8d985
lashner <lashner@umich.edu>
2018-02-16 15:20:51 -0500

    Update groove database before generation

--- harmonaize/python/generate_midi.py
+++ harmonaize/python/generate_midi.py
@@ -12,12 +12,12 @@ def genAccompaniment(midi=None):
 	generator = ChordProg(FileAttributes['key'], "generated_files/accomp.mma", FileAttributes['tempo'], FileAttributes['groove'])
 	generator.generateMMAFormat()
 
-	generated = genMidi("generated_files/accomp.mma")
+	genMidi("generated_files/accomp.mma")
 
 	return mido.MidiFile("generated_files/accomp.mid")
 
 def genMidi(path_to_fakebook):
-	os.system('python3 ../external/mma/mma.py ' + path_to_fakebook)
+	os.system('../external/mma/mma.py -g && python3 ../external/mma/mma.py ' + path_to_fakebook)
 
 if __name__ == '__main__':
 	midi_file = mido.MidiFile('../../app/Design/test.mid')
