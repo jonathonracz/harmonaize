ea83c8b08d3b87fa29405287cae49e9a66dd01a4
Landon Kartes <landonkartes@gmail.com>
2018-03-13 12:02:47 -0400

    Menu shows up

--- app/Source/hmnz_EditWindow.cpp
+++ app/Source/hmnz_EditWindow.cpp
@@ -17,10 +17,18 @@ EditWindow::EditWindow()
                       DocumentWindow::allButtons)
 {
     setUsingNativeTitleBar (true);
-    setResizeLimits(800, 600, std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
+    setResizeLimits (800, 600, std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
     setContentOwned (new EditView(), false);
     centreWithSize (getWidth(), getHeight());
     setVisible (true);
+    
+    setApplicationCommandManagerToWatch (&HarmonaizeApplication::getCommandManager());
+    #if JUCE_MAC
+    PopupMenu extraAppleMenuItems;
+    extraAppleMenuItems.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::showPreferences);
+    setMacMainMenu (this, &extraAppleMenuItems);
+    #endif
+    HarmonaizeApplication::getCommandManager().setFirstCommandTarget (this);
 
     ValueTree defaultEdit = Edit::createDefaultState();
     setEdit (defaultEdit);
@@ -52,4 +60,156 @@ void EditWindow::setEdit (const ValueTree& edit)
         playbackEngine.setEdit (currentEdit.get());
         static_cast<EditView*> (getContentComponent())->setEdit (currentEdit.get());
     }
+}
+
+StringArray EditWindow::getMenuBarNames()
+{
+    StringArray names;
+    names.add ("File");
+    names.add ("Edit");
+    return names;
+}
+
+PopupMenu EditWindow::getMenuForIndex (int topLevelMenuIndex, const String& /*menuName*/)
+{
+    PopupMenu menu;
+    
+    if (topLevelMenuIndex == 0)
+    {
+        // "File" menu
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::newProject);
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::openProject);
+        
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::saveProject);
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::saveProjectAs);
+    }
+    else if (topLevelMenuIndex == 1)
+    {
+        // "Edit" menu
+        PopupMenu editMenu;
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::undo);
+        menu.addCommandItem (&HarmonaizeApplication::getCommandManager(), CommandIDs::redo);
+    }
+    
+    return menu;
+}
+
+void EditWindow::menuItemSelected (int menuItemID, int /*topLevelMenuIndex*/)
+{
+    if (menuItemID == 250)
+    {
+    }
+    else if (menuItemID >= 100 && menuItemID < 200)
+    {
+    }
+    else
+    {
+    }
+}
+
+void EditWindow::getAllCommands (Array<CommandID>& commands)
+{
+    // this returns the set of all commands that this target can perform..
+    const CommandID ids[] = { CommandIDs::newProject,
+        CommandIDs::openProject,
+        CommandIDs::saveProject,
+        CommandIDs::saveProjectAs,
+        CommandIDs::showPreferences,
+        CommandIDs::undo,
+        CommandIDs::redo
+    };
+    
+    commands.addArray (ids, numElementsInArray (ids));
+}
+
+ApplicationCommandTarget* EditWindow::getNextCommandTarget()
+{
+    return findFirstTargetParentComponent();
+}
+
+void EditWindow::getCommandInfo (const CommandID commandID, ApplicationCommandInfo& result)
+{
+    const String category ("General");
+    
+    switch (commandID)
+    {
+        case CommandIDs::newProject:
+            result.setInfo ("New", "Creates a new filter graph file", category, 0);
+            result.defaultKeypresses.add(KeyPress('n', ModifierKeys::commandModifier, 0));
+            break;
+            
+        case CommandIDs::openProject:
+            result.setInfo ("Open...", "Opens a filter graph file", category, 0);
+            result.defaultKeypresses.add (KeyPress ('o', ModifierKeys::commandModifier, 0));
+            break;
+            
+        case CommandIDs::saveProject:
+            result.setInfo ("Save", "Saves the current graph to a file", category, 0);
+            result.defaultKeypresses.add (KeyPress ('s', ModifierKeys::commandModifier, 0));
+            break;
+            
+        case CommandIDs::saveProjectAs:
+            result.setInfo ("Save As...",
+                            "Saves a copy of the current graph to a file",
+                            category, 0);
+            result.defaultKeypresses.add (KeyPress ('s', ModifierKeys::shiftModifier | ModifierKeys::commandModifier, 0));
+            break;
+            
+        case CommandIDs::showPreferences:
+            result.setInfo ("Preferences", String(), category, 0);
+            result.addDefaultKeypress ('p', ModifierKeys::commandModifier);
+            break;
+            
+        case CommandIDs::undo:
+            result.setInfo ("Undo", String(), category, 0);
+            result.addDefaultKeypress ('z', ModifierKeys::commandModifier);
+            break;
+            
+        case CommandIDs::redo:
+            result.setInfo ("Redo", String(), category, 0);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+bool EditWindow::perform (const InvocationInfo& info)
+{
+    switch (info.commandID)
+    {
+        case CommandIDs::newProject: {
+            //            if (graphHolder != nullptr && graphHolder->graph != nullptr && graphHolder->graph->saveIfNeededAndUserAgrees() == FileBasedDocument::savedOk)
+            //                graphHolder->graph->newDocument();
+            break;
+        }
+        case CommandIDs::openProject: {
+            //            if (graphHolder != nullptr && graphHolder->graph != nullptr && graphHolder->graph->saveIfNeededAndUserAgrees() == FileBasedDocument::savedOk)
+            //                graphHolder->graph->loadFromUserSpecifiedFile (true);
+            break;
+        }
+        case CommandIDs::saveProject: {
+            //            if (graphHolder != nullptr && graphHolder->graph != nullptr)
+            //                graphHolder->graph->save (true, true);
+            break;
+        }
+        case CommandIDs::saveProjectAs: {
+            //            if (graphHolder != nullptr && graphHolder->graph != nullptr)
+            //                graphHolder->graph->saveAs (File(), true, true, true);
+            break;
+        }
+        case CommandIDs::showPreferences: {
+            break;
+        }
+        case CommandIDs::undo: {
+            break;
+        }
+        case CommandIDs::redo: {
+            break;
+        }
+        default:
+            return false;
+    }
+    
+    return true;
 }
