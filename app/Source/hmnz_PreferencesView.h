a6c881fb30c1cc8ba86ac3cafe5b077e1843c388
Landon Kartes <landonkartes@gmail.com>
2018-03-14 15:05:15 -0400

    Added scaling

--- app/Source/hmnz_PreferencesView.h
+++ app/Source/hmnz_PreferencesView.h
@@ -12,17 +12,23 @@
 
 #include "JuceHeader.h"
 
-class PreferencesView   : public Component
+class PreferencesView   : public Component,
+                          public Slider::Listener
 {
 public:
     PreferencesView();
+    void scaleUp();
+    void scaleDown();
 
 private:
     AudioDeviceSelectorComponent selectorComponent;
+    Slider scaleSlider;
+    Label scaleLabel;
 
     void resized() override;
     void paint (Graphics&) override;
     void userTriedToCloseWindow() override;
+    void sliderValueChanged(Slider* slider) override;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PreferencesView);
 };
