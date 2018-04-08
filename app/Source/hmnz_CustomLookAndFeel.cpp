a45df95cfce40ed430f296383e9d22ba2960a479
Landon Kartes <landonkartes@gmail.com>
2018-04-08 14:41:19 -0400

    Added custom look and feel class

---
+++ app/Source/hmnz_CustomLookAndFeel.cpp
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    CustomLookAndFeel.cpp
+    Created: 8 Apr 2018 12:25:02pm
+    Author:  Landon
+
+  ==============================================================================
+*/
+
+#include "hmnz_CustomLookAndFeel.h"
+
+
+CustomLookAndFeel::CustomLookAndFeel()
+{
+    setDefaultSansSerifTypefaceName("Lucida Sans Unicode");
+    fontScale = 1;
+}
+
+CustomLookAndFeel::~CustomLookAndFeel()
+{
+    
+}
+
+Font CustomLookAndFeel::getComboBoxFont (ComboBox&)
+{
+    return Font (15 * fontScale);
+}
+
+Font CustomLookAndFeel::getTextButtonFont (TextButton& textButton, int buttonHeight)
+{
+    return Font (15 * fontScale);
+}
+
+Font CustomLookAndFeel::getLabelFont (Label& label)
+{
+    return Font (15 * fontScale);
+}
+
+Font CustomLookAndFeel::getSliderPopupFont (Slider& slider)
+{
+    return Font (15 * fontScale);
+}
+
+void CustomLookAndFeel::setFontScale(double scale)
+{
+    fontScale = scale;
+}
+
+double CustomLookAndFeel::getFontScale()
+{
+    return fontScale;
+}
---
+++ app/Source/hmnz_CustomLookAndFeel.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    CustomLookAndFeel.h
+    Created: 8 Apr 2018 12:25:02pm
+    Author:  Landon
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+
+class CustomLookAndFeel : public LookAndFeel_V4
+{
+public:
+    CustomLookAndFeel();
+    virtual ~CustomLookAndFeel();
+    void setFontScale(double scale);
+    double getFontScale();
+private:
+    virtual Font getComboBoxFont (ComboBox&);
+    virtual Font getTextButtonFont (TextButton&, int buttonHeight);
+    virtual Font getLabelFont (Label&);
+    virtual Font getSliderPopupFont (Slider&);
+
+    double fontScale;
+};
